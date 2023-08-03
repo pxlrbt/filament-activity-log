d7479a8901934fc29831f0463ad79572f19c5d09
Tony Partridge <admin@xtech.im>
2023-08-03 12:16:49 +0100

    Migrate FilamentActivityLogServiceProvider

--- src/FilamentActivityLogServiceProvider.php
+++ src/FilamentActivityLogServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace pxlrbt\FilamentActivityLog;
 
-use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentActivityLogServiceProvider extends PluginServiceProvider
+class FilamentActivityLogServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-activity-log';
 
