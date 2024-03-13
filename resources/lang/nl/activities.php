0262aba6c122bd8472ceab36f44f75a1261712e5
Simon Broekaert <simon_broekaert@hotmail.com>
2024-03-13 21:30:57 +0100

    Add dutch (nl) translations

---
+++ resources/lang/nl/activities.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'breadcrumb' => 'Geschiedenis',
+
+    'title' => 'Geschiedenis :record',
+
+    'default_datetime_format' => 'Y-m-d, H:i:s',
+
+    'table' => [
+        'field' => 'Veld',
+        'old' => 'Oud',
+        'new' => 'Nieuw',
+        'restore' => 'Herstellen',
+    ],
+
+    'events' => [
+        'updated' => 'Bewerkt',
+        'created' => 'Aangemaakt',
+        'deleted' => 'Verwijderd',
+        'restored' => 'Hersteld',
+        'restore_successful' => 'Succesvol hersteld',
+        'restore_failed' => 'Herstellen mislukt',
+    ],
+];
