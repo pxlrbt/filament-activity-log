5bc72c6914cd2f33d4d9eb944456bc016c7017fb
Marc-Antoine Favreau <8480549+Heyian@users.noreply.github.com>
2024-06-16 15:33:25 -0400

    Added French translations

---
+++ resources/lang/fr/activities.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'breadcrumb' => 'Historique',
+
+    'title' => 'Historique :record',
+
+    'default_datetime_format' => 'Y-m-d, H:i:s',
+
+    'table' => [
+        'field' => 'Champ',
+        'old' => 'Ancien',
+        'new' => 'Nouveau',
+        'restore' => 'Restaurer',
+    ],
+
+    'events' => [
+        'updated' => 'Mis à jour',
+        'created' => 'Créé',
+        'deleted' => 'Effacé',
+        'restored' => 'Restauré',
+        'restore_successful' => 'Restauré avec succès',
+        'restore_failed' => 'Échec de la restauration',
+    ],
+];
