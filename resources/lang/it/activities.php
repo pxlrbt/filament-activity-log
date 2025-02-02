9956fa3ed645c46e94bbaf6bb3faecf34a41d200
pxlrbt <pxlrbt@users.noreply.github.com>
2025-02-02 05:26:01 +0000

    Apply style changes

--- resources/lang/it/activities.php
+++ resources/lang/it/activities.php
@@ -1,25 +1,25 @@
 <?php
 
 return [
-	'breadcrumb' => 'Cronologia',
+    'breadcrumb' => 'Cronologia',
 
-	'title' => 'Cronologia :record',
+    'title' => 'Cronologia :record',
 
-	'default_datetime_format' => 'd/m/Y, H:i:s',
+    'default_datetime_format' => 'd/m/Y, H:i:s',
 
-	'table' => [
-		'field' => 'Campo',
-		'old' => 'Vecchio',
-		'new' => 'Nuovo',
-		'restore' => 'Ripristina',
-	],
+    'table' => [
+        'field' => 'Campo',
+        'old' => 'Vecchio',
+        'new' => 'Nuovo',
+        'restore' => 'Ripristina',
+    ],
 
-	'events' => [
-		'updated' => 'Aggiornato',
-		'created' => 'Creato',
-		'deleted' => 'Eliminato',
-		'restored' => 'Ripristinato',
-		'restore_successful' => 'Ripristinato con successo',
-		'restore_failed' => 'Ripristino fallito',
-	],
+    'events' => [
+        'updated' => 'Aggiornato',
+        'created' => 'Creato',
+        'deleted' => 'Eliminato',
+        'restored' => 'Ripristinato',
+        'restore_successful' => 'Ripristinato con successo',
+        'restore_failed' => 'Ripristino fallito',
+    ],
 ];
